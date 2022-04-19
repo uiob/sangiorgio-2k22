15d48686bf0ea32014dc1a14ba063ddcad731efa
uiob <uiob1210@gmail.com>
2022-04-19 21:43:20 +0200

    tweaks to decodifica

--- script/infoDecodificatore.zsh
+++ script/infoDecodificatore.zsh
@@ -1,17 +1,13 @@
 #!/bin/zsh
 clear 
 
-source printNotCentered.zsh
+source printAsciiArt.zsh
 
-printNotCentered ./../txt/istruzioniDecodificatore.txt
+printAscii ./../txt/istruzioniDecodificatore.txt
 
-echo "Premi un tasto qualsiasi per tornare al menu'..."
-while [ true ] ; do
-read -t 3 -n 1
-if [ $? = 0 ] ; then
-	zsh menu.zsh
-exit ;
-else
-echo "waiting for the keypress"
-fi
-done
+sleep 2
+
+echo -n "\n\nPremi un tasto qualsiasi per tornare al menu'... "
+read tasto
+
+zsh menu.zsh
